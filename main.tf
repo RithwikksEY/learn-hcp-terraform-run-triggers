cfa9f71300872f9306b2429fdb98e37d690c8ea0
RithwikksEY <Rithwik.Kumar.Sahu@gds.ey.com>
2026-03-02 12:39:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,7 @@
-provider "aws" {
-  region = "us-west-2"
+terraform {
+  required_version = ">= 1.5.0"
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-data "tfe_outputs" "source_workspace" {
-  workspace    = var.workspace_name
-  organization = var.organization_name
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = data.tfe_outputs.source_workspace.nonsensitive_values.instance_security_group_ids
-  subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
-
-  tags = {
-    Name = var.instance_name
-  }
+ 
+output "example" {
+  value = "hello"
 }
